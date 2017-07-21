458e0f3e7a018cf0643b86c12f6c0298c7b46a7d
joe <joepatmckenna@gmail.com>
2017-07-20 23:45:51 -0400

    changed python package link

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='normal_forms',
-    version='0.1',
+    version='0.3',
     description='Normal forms of dynamical systems',
     keywords='normal form differential equation dynamical system bifurcation',
-    url='https://github.com/joepatmckenna/normal_forms',
+    url='https://joepatmckenna.github.io/normal_forms/html',
     author='Joseph P. McKenna',
     author_email='joepatmckenna@gmail.com',
     license='MIT',
     packages=['normal_forms'],
-    install_requires=['numpy', 'sympy'],
+    install_requires=['numpy', 'sympy','copy'],
     zip_safe=False)
